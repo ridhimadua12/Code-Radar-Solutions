9d77b316d07a0fd8318c5512efca1a5039c8780a
ridhimadua12 <ridhima1640.becse24@chitkara.edu.in>
2025-02-11 12:42:44 +0530

    Solution to Check Even or Odd Using Division

--- Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
+++ Check Even or Odd Using Division@@6788dd63490637cfc44e1b9b/code.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main (){
-    int a , b ;
-    scanf("%d",&a,&b);
-    if (a)
+    int a ;
+    scanf("%d",&a);
+    if (a%=2){
+        printf ("Even");
+    }else{
+        printf("Odd")
+    }
+    return 0;
 }
